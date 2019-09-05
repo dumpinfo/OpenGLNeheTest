eb1c46b78dbdef7d61a34a410003babf07344480
ocean <dumpinfo@163.com>
2019-09-06 04:32:56 +0800

    add files

---
+++ NeHe OpenGL中文教程/Alias.h
@@ -0,0 +1,12 @@
+IDH_1066=t.htm	;1066
+IDH_1055=navigator.htm	;1055
+IDH_1002=Course\Tutorial_01.htm	;1002
+IDH_1003=Course\Tutorial_02.htm	;1003
+IDH_1004=Course\Tutorial_03.htm	;1004
+IDH_1005=Course\Tutorial_04.htm	;1005
+IDH_1006=Course\Tutorial_05.htm	;1006
+IDH_1007=Course\Tutorial_06.htm	;1007
+IDH_1008=Course\Tutorial_07.htm	;1008
+IDH_1009=Course\Tutorial_08.htm	;1009
+IDH_1010=Course\Tutorial_09.htm	;1010
+; ... UNREGISTERED ...
---
+++ NeHe OpenGL中文教程/Map.h
@@ -0,0 +1,12 @@
+#define IDH_1066	1066
+#define IDH_1055	1055
+#define IDH_1002	1002
+#define IDH_1003	1003
+#define IDH_1004	1004
+#define IDH_1005	1005
+#define IDH_1006	1006
+#define IDH_1007	1007
+#define IDH_1008	1008
+#define IDH_1009	1009
+#define IDH_1010	1010
+; ... UNREGISTERED ...
